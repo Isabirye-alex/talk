c6ce7f2094fe6d1ab6ba76346f35284b9ca4ab43
Isabirye-alex <alexisabirye558@gmail.com>
2025-06-24 12:20:24 +0300

    step 2

---
+++ lib/UI/fetch_data_class.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FetchDataClass extends StatefulWidget {
+  const FetchDataClass({super.key});
+
+  @override
+  State<FetchDataClass> createState() => _FetchDataClassState();
+}
+
+class _FetchDataClassState extends State<FetchDataClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
